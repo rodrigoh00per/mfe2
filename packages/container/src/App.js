e9ddcdaa05bd2b38e34061af3ffea21e7f290d25
Rodrigo Guerrero <rodrigoh00per1@gmail.com>
2021-03-02 22:04:57 -0600

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+const App = () => {
+  return (
+    <div>
+      <h3>Texto del contenedor</h3>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+};
+
+export default App;
